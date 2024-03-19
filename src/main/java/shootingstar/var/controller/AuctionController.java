c386352a186cd38df6765c4db99aa2d6a20fc6a0
Minjeong63 <kmj06030@naver.com>
2024-03-19 15:35:01 +0900

    Feat: 경매 생성 API에 swagger 적용

--- src/main/java/shootingstar/var/controller/AuctionController.java
+++ src/main/java/shootingstar/var/controller/AuctionController.java
@@ -1,5 +1,9 @@
 package shootingstar.var.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import shootingstar.var.Service.AuctionService;
 import shootingstar.var.dto.req.AuctionCreateReqDto;
 
+@Tag(name = "경매 컨트롤러", description= "vip 권한만 사용 가능합니다.")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/vip/auction")
@@ -18,6 +23,10 @@ public class AuctionController {
 
     private final AuctionService auctionService;
 
+    @Operation(summary = "경매 생성 API", description = "VIP 권한을 가진 사용자가 경매를 생성할 때 사용합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "경매 생성 성공")
+    })
     @PostMapping("/create")
     public ResponseEntity<String> create(@Valid @RequestBody AuctionCreateReqDto reqDto, HttpServletRequest request) {
         auctionService.create(reqDto, request);
