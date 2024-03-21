feac6df67746521fc3b2ab43a56b9f7afe9acc91
dnjs2721 <skadu66@gmail.com>
2024-03-21 18:00:52 +0900

    Feat : 중복 로그인 방지 추가
    액세스 토큰 만료시간은 30분으로 잡을 예정입니다.
    
    - 중복 로그인 방지를 위해 로그인 리스트를 저장하는 레디스 추가
    - 로그인시 로그인 리스트에서 해당 사용자를 찾고 존재한다면 이전 리프레시 토큰을 만료시키고
      로그인 리스트에 상태를 업데이트 합니다.
    - 로그인 리스트에 해당 사용자가 없다면 로그인 리스트에 추가합니다.
    - 로그인 리스트 레디스와 토큰 레디스에 저장되는 값은 해당하는 리프레시 토큰의 생명주기와 같습니다.
      레디스에서 생명주기가 끝난 데이터는 자동으로 삭제됩니다.
    
    - 액세스 토큰 재발급시 현재 로그인 리스트에 등록된 리프레시 토큰 정보가 다르다면
      다른 장소에서 로그인 되었다는 에러를 반환합니다.
    - 토큰 재발급시 토큰 관련 에러가 발생했을 경우 쿠키에 존재하는 토큰을 제거합니다.
    
    - 로그아웃의 경우 리프레시 토큰을 검증하지만 에러를 반환하지 않습니다.
      로그아웃 행위의 목적은 리프레시 토큰 만료가 목적이기에
      로그만 남기고 성공했다고 반환합니다.
    - 토큰 문제 없이 로그아웃에 성공하였을경우 로그인 리스트에서 해당 사용자를 삭제합니다.
    - 로그아웃 프로세스가 종료되면 쿠키에 존재하는 토큰을 제거합니다.

--- src/main/java/shootingstar/var/Service/UserAuthService.java
+++ src/main/java/shootingstar/var/Service/UserAuthService.java
@@ -10,12 +10,15 @@ import shootingstar.var.entity.User;
 import shootingstar.var.exception.CustomException;
 import shootingstar.var.jwt.JwtTokenProvider;
 import shootingstar.var.repository.UserRepository;
+import shootingstar.var.util.LoginListRedisUtil;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import static shootingstar.var.exception.ErrorCode.INVALID_REFRESH_TOKEN;
+import static shootingstar.var.exception.ErrorCode.LOGGED_IN_SOMEWHERE_ELSE;
 
 @Service
 @RequiredArgsConstructor
@@ -23,14 +26,22 @@ public class UserAuthService {
 
     private final UserRepository userRepository;
     private final JwtTokenProvider tokenProvider;
+    private final LoginListRedisUtil loginListRedisUtil;
 
     public String refreshAccessToken(String refreshToken) {
-        if (refreshToken == null) {
-            throw new CustomException(INVALID_REFRESH_TOKEN);
-        }
-        tokenProvider.validateRefreshToken(refreshToken);
-        tokenProvider.checkRefreshTokenState(refreshToken);
+        tokenProvider.validateRefreshToken(refreshToken); // 토큰 위변조 체크
         Authentication authentication = tokenProvider.getAuthenticationFromRefreshToken(refreshToken);
+
+        String userUUID = authentication.getName();
+        String storeRefreshToken = loginListRedisUtil.getData(userUUID);
+        // 현재 로그인 리스트에 등록된 토큰과 다를 경우
+        // 현재 로그인 리스트에 등록되진 않았지만 리프레시 토큰을 들고 있는 경우는 다른 곳에서 로그인한 뒤 그곳에서 로그아웃을 한 경우이다.
+        // 위 두 경우 모두 다른 곳에서 로그인 되었다는 뜻이기에 에러를 반환
+        if (!Objects.equals(storeRefreshToken, refreshToken)) {
+            throw new CustomException(LOGGED_IN_SOMEWHERE_ELSE);
+        }
+
+        tokenProvider.checkRefreshTokenState(refreshToken);
 
         return tokenProvider.generateAccessToken(authentication, Instant.now());
     }
@@ -50,5 +61,21 @@ public class UserAuthService {
 
         // 존재하지 않는 사용자인 경우 다른 로직을 처리할 수 있도록 null 반환
         return null;
+    }
+
+    public void logout(String refreshToken) {
+        tokenProvider.validateRefreshToken(refreshToken);
+
+        Authentication authentication = tokenProvider.getAuthenticationFromRefreshToken(refreshToken);
+
+        String userUUID = authentication.getName();
+
+        tokenProvider.expiredRefreshTokenAtRedis(refreshToken); // 현재 토큰을 만료 처리한다
+
+        String storeRefreshToken = loginListRedisUtil.getData(userUUID); // 로그인 리스트에 등록된 토큰
+        if (!Objects.equals(storeRefreshToken, refreshToken)) { // 로그인 리스트와 현재 등록된 토큰이 다를 경우
+            tokenProvider.expiredRefreshTokenAtRedis(storeRefreshToken); // 로그인 리스트에 등록된 토큰도 만료 시킨다.
+        }
+        loginListRedisUtil.deleteData(userUUID); // 로그인 리스트에서 사용자 정보를 제거한다.
     }
 }
