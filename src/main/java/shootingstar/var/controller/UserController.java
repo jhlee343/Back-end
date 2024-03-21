f2b8e4fcccff8ce9b6d6fa8e994b8f76c60418b8
reenact_l <77737814+jhlee343@users.noreply.github.com>
2024-03-21 13:28:32 +0900

    Fix : userConroller swagger Tag

--- src/main/java/shootingstar/var/controller/UserController.java
+++ src/main/java/shootingstar/var/controller/UserController.java
@@ -1,5 +1,8 @@
 package shootingstar.var.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
@@ -26,35 +29,46 @@ public class UserController {
     private final UserService userService;
     private final JwtTokenProvider jwtTokenProvider;
 
+    @Operation(summary = "프로필 불러오기")
     @GetMapping("/profile/{nickname}")
     public ResponseEntity<UserProfileDto> getProfile(@PathVariable String nickname) {
         UserProfileDto profile = userService.getProfile(nickname);
         return ResponseEntity.ok(profile);
     }
 
+    @Operation(summary = "VIP 확인")
     @GetMapping("/checkVIP")
     public ResponseEntity<Boolean> checkVIP(HttpServletRequest request) {
         return ResponseEntity.ok(userService.checkVIP(request));
     }
 
+    @Operation(summary = "팔로우 리스트 불러오기")
     @GetMapping("/followingList")
     public ResponseEntity<?> followingList(HttpServletRequest request) {
         String userUUID = jwtTokenProvider.getUserUUIDByRequest(request);
         List<FollowingDto> followingList = userService.findAllFollowing(userUUID);
         return ResponseEntity.ok().body(followingList);
     }
+    @Operation(summary = "팔로우하기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "follow success")
+    })
     @GetMapping("/follow/{followingId}")
     public ResponseEntity<String> follow(@NotBlank @PathVariable("followingId") String followingId, HttpServletRequest request) {
         String userUUID = jwtTokenProvider.getUserUUIDByRequest(request);
         userService.follow(followingId,userUUID);
         return ResponseEntity.ok("follow success");
     }
+    @Operation(summary = "언팔로우")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "unfollow success")
+    })
     @DeleteMapping("/unfollow/{followUUID}")
     public ResponseEntity<String> unFollow(@NotBlank @PathVariable("followUUID") String followUUID) {
         userService.unFollow(followUUID);
         return ResponseEntity.ok().body("unfollow success");
     }
-
+    @Operation(summary = "경고 내역")
     @GetMapping("warningList")
     public ResponseEntity<WarningListDto> warningList(HttpServletRequest request){
         String userUUID = jwtTokenProvider.getUserUUIDByRequest(request);
